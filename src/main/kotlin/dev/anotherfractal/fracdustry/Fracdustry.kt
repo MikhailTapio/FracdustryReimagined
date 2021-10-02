00f96b06b37098b8dd0757404aae36939e7128da
Peakstep <62203463+Peakstep233@users.noreply.github.com>
2021-10-02 21:21:17 +0800

    Init Part II

---
+++ src/main/kotlin/dev/anotherfractal/fracdustry/Fracdustry.kt
@@ -0,0 +1,13 @@
+package dev.anotherfractal.fracdustry
+
+
+
+@Suppress("unused")
+fun init() {
+    // This code runs as soon as Minecraft is in a mod-load-ready state.
+    // However, some things (like resources) may still be uninitialized.
+    // Proceed with mild caution.
+
+    println("Hello Fabric world!")
+}
+
