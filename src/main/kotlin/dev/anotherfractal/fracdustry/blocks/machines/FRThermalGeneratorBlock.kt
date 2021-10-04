00312b696a62217b68e9e36c33020f225d188b64
Peakstep <62203463+Peakstep233@users.noreply.github.com>
2021-10-04 21:32:10 +0800

    [Chore] BlockStates, VoxelShapes and an ambulance
    BlockStates for Thermal Generator completed
    VoxelShapes WIP

--- src/main/kotlin/dev/anotherfractal/fracdustry/blocks/machines/FRThermalGeneratorBlock.kt
+++ src/main/kotlin/dev/anotherfractal/fracdustry/blocks/machines/FRThermalGeneratorBlock.kt
@@ -1,11 +1,19 @@
 package dev.anotherfractal.fracdustry.blocks.machines
 
 import net.minecraft.block.Block
+import net.minecraft.block.BlockState
+import net.minecraft.block.HorizontalFacingBlock.FACING
+import net.minecraft.block.ShapeContext
 import net.minecraft.client.item.TooltipContext
+import net.minecraft.item.ItemPlacementContext
 import net.minecraft.item.ItemStack
+import net.minecraft.state.StateManager
 import net.minecraft.text.Text
 import net.minecraft.text.TranslatableText
 import net.minecraft.util.Formatting
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.util.shape.VoxelShapes
 import net.minecraft.world.BlockView
 
 
@@ -18,5 +26,20 @@ class FRThermalGeneratorBlock(settings: Settings?) : Block(settings) {
     ) {
         tooltip.add(TranslatableText("tooltips.fracdustry.generator1").formatted(Formatting.GRAY))
         tooltip.add(TranslatableText("tooltips.fracdustry.generator2").formatted(Formatting.GRAY))
+    }
+    override fun appendProperties(builder: StateManager.Builder<Block, BlockState>?) {
+        super.appendProperties(builder)
+        if (builder != null) builder.add(FACING)
+    }
+    override fun getPlacementState(ctx: ItemPlacementContext): BlockState? {
+        return defaultState.with(FACING, ctx.playerFacing.opposite)
+    }
+    override fun getOutlineShape(
+        state: BlockState?,
+        view: BlockView?,
+        pos: BlockPos?,
+        context: ShapeContext?
+    ): VoxelShape? {
+        return VoxelShapes.cuboid(0.01, 0.0, 0.01, 0.99, 1.0, 0.99)
     }
 }
